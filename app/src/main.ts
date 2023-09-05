4668b079ae7b53ea0a62888302577676c3173fe7
furuTra <tktk3008take@gmail.com>
2023-09-06 00:38:24 +0900

    swaggerを導入する

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ["error", "warn", "log"]
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('example')
+    .setDescription('NestJSとPrisma, TiDBでのAPIのサンプル')
+    .setVersion('0.0.0')
+    .addTag('example')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
